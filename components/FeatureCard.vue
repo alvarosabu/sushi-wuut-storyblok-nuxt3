dec3de4af6a1563ce49d18e642189973195a9569
Alvaro Saburido <alvaro.saburido@gmail.com>
2021-11-30 18:36:41 +0100

    feat: feature sections

---
+++ components/FeatureCard.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+<template>
+  <li class="p-4 flex flex-col items-center rounded-lg border-2 border-gray-50">
+    <img class="w-16 h-16 mb-4" :src="blok.icon.filename" :alt="blok.label" />
+    <span class="font-display font-font-extrabold text-sm text-gray-50">{{
+      blok.label
+    }}</span>
+  </li>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ components/TheFeatures.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+const props = defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+<template>
+  <section class="feature-list bg-shrimp-400 py-16">
+    <div class="container mx-auto flex flex-col sm:flex-row">
+      <div class="w-1/2 flex flex-col justify-between">
+        <h2 class="font-extrabold font-display text-3xl text-gray-50 mb-8">
+          {{ blok.headline }}
+        </h2>
+        <ul
+          class="py-8 grid grid-cols-3 gap-4"
+          v-if="blok.features_list.length > 0"
+        >
+          <component
+            v-for="feature in blok.features_list"
+            :key="feature._uid"
+            :is="feature.component"
+            :blok="feature"
+          />
+        </ul>
+      </div>
+      <div class="w-1/2 flex justify-end">
+        <img
+          v-if="blok.media"
+          :src="blok.media.filename"
+          :alt="blok.media.filename.alt"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
